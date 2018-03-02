99fa7b3bfdee04e5d5261e2a393ca8435e1d9c4e
Dang Diep <diepdangvan.alt@gmail.com>
2018-03-02 11:44:47 +0700

    update

--- src/LogController.php
+++ src/LogController.php
@@ -9,7 +9,7 @@ use Illuminate\Routing\Controller;
 
 class LogController extends Controller
 {
-    public function index($file, Request $request)
+    public function index($file = null, Request $request)
     {
         return Admin::content(function (Content $content) use ($file, $request) {
             $offset = $request->get('offset');
